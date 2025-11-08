29b681285f841257e206a75d456f4d8b4b368bd6
phunnipath.t@ku.th <phunnipath.t@ku.th>
2025-11-08 14:04:26 +0700

    added Book class

--- oop_solution/library_oop.py
+++ oop_solution/library_oop.py
@@ -1,6 +1,15 @@
 # Library Management System - Procedural Style
 
 
+
+class Book():
+    def __init__(self,id, title, author, total_copies, available_copies):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.total_copies = total_copies
+        self.available_copies = available_copies
+
 class Member():
     def __init__(self,id, name, email, borrowed_books):
         self.id = id
